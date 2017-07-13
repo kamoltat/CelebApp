ef9c047efbd79e72eb8ecd3b35ae316221c4ac7f
chadakeng <chada.keng@gmail.com>
2017-07-13 11:04:46 +0700

    search first commit

--- CelebApp1/src/pages/search/search.ts
+++ CelebApp1/src/pages/search/search.ts
@@ -1,17 +1,22 @@
+import {FirebaseProvider} from '../../providers/firebase';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-about',
-  templateUrl: 'search.html'
+  templateUrl: 'search.html',
+  providers:[FirebaseProvider]
 })
-export class SearchPage {
-searchQuery: string = '';
-  items: string[];
-  constructor(public navCtrl: NavController) {
- 
 
- 
+export class SearchPage {
+  searchQuery: string = '';
+  items: string[];
+  shoppingItems: FirebaseListObservable<any[]>
+  newItem ='';
+
+  constructor(public navCtrl: NavController, public firebaseProvider: FirebaseProvider) {
+    this.shoppingItems = this.firebaseProvider.getShoppingItems();
     this.initializeItems();
   }
 
@@ -37,6 +42,15 @@ searchQuery: string = '';
       })
     }
   }
+ 
+  addItem() {
+    this.firebaseProvider.addItem(this.newItem);
+  }
+ 
+  removeItem(id) {
+    this.firebaseProvider.removeItem(id);
+  }
+
   }
 
 
