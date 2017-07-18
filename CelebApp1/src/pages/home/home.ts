87ec44fb2361d7c843d8438a2ac2880887be6377
kamoltat <ksirivad@bu.edu>
2017-07-18 10:12:14 +0700

    rearranged firebase, integrate useremail and password to

--- CelebApp1/src/pages/home/home.ts
+++ CelebApp1/src/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component, NgZone } from '@angular/core';
 import {IonicPage, NavController, NavParams,ToastController } from 'ionic-angular';
 import {UserServiceProvider} from '../../providers/user-service/user-service'
-import firebase from 'firebase';
 import {AngularFireAuth} from 'angularfire2/auth';
 import {AngularFireDatabase, FirebaseObjectObservable,FirebaseListObservable} from 'angularfire2/database';
 import {CreateProfilePage} from '../create-profile/create-profile';
 import {PostPage} from "../post/post";
+import {LoginPage} from "../login/login";
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
@@ -15,62 +16,38 @@ import {PostPage} from "../post/post";
 
 
 export class HomePage {
-  profileData : FirebaseObjectObservable<CreateProfilePage>
-  database:any
-  celeb1 :any
-  
+  profileData : FirebaseObjectObservable<CreateProfilePage>;
+  database:any;
+  public userProfile:any;
   
   
   constructor(public navCtrl: NavController, 
   public userProvider: UserServiceProvider, 
-  // public af: AngularFire,
   public afAuth: AngularFireAuth, private toastCtrl: ToastController, 
   private afDatabase: AngularFireDatabase, public zone: NgZone) {
-  
-}
-
-
-status = this.getCelebStatus();
-
-
-
-  
-
-getCelebStatus(){
-  var user = firebase.auth().currentUser;
-  if(user){
-  return firebase.database().ref('profile/' + user.uid + '/is_celeb').once('value').then(function(snapshot) {
-  var celeb = snapshot.val(); 
-  console.log(user.email)
-  console.log(celeb)
-  console.log("user signed in")
-  })
-  }
-  else {
-  console.log("No user signed in")
-}
-}
-
-
-
-
-ionViewWillLoad(){
-  this.afAuth.authState.subscribe(data => {
-  if(data && data.email && data.uid){
     
-    
-  }
+}
 
 
 
 
-  // else{
-  //   this.toastCtrl.create({
-  //   message: 'Could not find authentication detail',
-  //   duration: 3000
-  // }).present();
-  // }
-  })
+
+
+ionViewDidEnter(){
+
+var user = firebase.auth().currentUser;
+if(user && user.uid && user.email){
+
+firebase.database().ref('profile/' + user.uid).on('value', snapshot => {
+this.userProfile = snapshot.val();
+});
+}
+
+else{
+  this.navCtrl.setRoot(LoginPage);
+}
+console.log(this.userProfile);
+
 }
 
 
