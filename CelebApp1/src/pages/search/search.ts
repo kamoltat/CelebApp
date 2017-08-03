c56095247a30d94478dcf049d29f0b5960beea31
chadakeng <chada.keng@gmail.com>
2017-08-03 17:54:31 +0700

    follow/unfollow button now changes style. Moved functions in search page to search provider instead.

--- CelebApp1/src/pages/search/search.ts
+++ CelebApp1/src/pages/search/search.ts
@@ -1,12 +1,12 @@
 import { SearchProvider } from '../../providers/search-service/search-service';
-import { Component, NgZone, OnInit,Injectable } from '@angular/core';
+import { Component, NgZone, OnInit, Injectable } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AngularFireModule } from 'angularfire2';
 import { FirebaseListObservable } from 'angularfire2/database';
 
 import firebase from 'firebase';
 import { IdolServiceProvider } from '../../providers/idol-service/idol-service';
-import {LoginPage } from '../login/login';
+import { LoginPage } from '../login/login';
 import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database'
 import { AngularFireAuth } from 'angularfire2/auth';
 import { TempProfilePage } from '../temp-profile/temp-profile';
@@ -16,172 +16,107 @@ import { SubjectProvider } from '../../providers/subject-service/subject-service
 @Component({
   selector: 'page-about',
   templateUrl: 'search.html',
-  providers:[SearchProvider]
+  providers: [SearchProvider]
 })
 
 @Injectable()
 export class SearchPage implements OnInit {
   searchQuery: string = '';
-  newItem ='';
+  newItem = '';
   storageRef = firebase.storage().ref();
   image: any;
   public items = new Array();
-  public idolKey= new Array();
+  public idolKey = new Array();
   public idolArray: any;
-  public followData:any={};
+  public followData: any = {};
+  followStatus:string;
   // is_following:number;
-  idolUID:string;
+  idolUID: string;
 
-  constructor(public navCtrl: NavController, public firebaseProvider: SearchProvider,
-    public af: AngularFireModule, public zone:NgZone, public navParams: NavParams,
-    private _subjectProvider: SubjectProvider){
-    
+  constructor(public navCtrl: NavController, public searchProvider: SearchProvider,
+    public af: AngularFireModule, public zone: NgZone, public navParams: NavParams,
+    private _subjectProvider: SubjectProvider) {
 
-    console.log("I'm in search page")
-  
   }
 
-  // getImage(childData){
-  //   this.storageRef.child(childData['profile_pic_url']).getDownloadURL().then((url)=>{
-  //     this.zone.run(() =>{ this.image = url;
-  //   });
-  //   });
-  //   return this.image;
-  // }
-
-
   //Getting idols from firebase
-  getIdols(){
-    var temp:any={};    
+  getIdols() {
+    var temp: any = {};
     var query = firebase.database().ref("idols").orderByKey();
     query.once("value")
-    .then(snapshot => {
-    snapshot.forEach(childSnapshot => {
-      var key = childSnapshot.key;
-      this.idolUID = key
-      // childData will be the actual contents of the child
-      var childData = childSnapshot.val();
-      temp.idolKey = key;
-      this.storageRef.child(childData['profile_pic_url']).getDownloadURL().then((url)=>{
-      this.zone.run(() =>{ childData['profile_pic_url'] = url;
-    }).catch(e=>{
+      .then(snapshot => {
+        snapshot.forEach(childSnapshot => {
+          var key = childSnapshot.key;
+          this.idolUID = key
+          // childData will be the actual contents of the child
+          var childData = childSnapshot.val();
+          temp.idolKey = key;
+          this.storageRef.child(childData['profile_pic_url']).getDownloadURL().then((url) => {
+            this.zone.run(() => {
+            childData['profile_pic_url'] = url;
+            }).catch(e => {
+              console.log("");
+            });
+          }).catch(e => {
+            console.log("");
+          });
+          //merge two objects together (this is to add idol Key into the object of the idol itself.)
+          var finalData = Object.assign(childData, temp);
+          delete finalData.password;
+          //pushing merged data to items
+          this.items.push(finalData);
+        }).catch(e => {
           console.log("");
         });
-    }).catch(e=>{
-          console.log("");
-        });
-      //merge two objects together (this is to add idol Key into the object of the idol itself.)
-      var finalData = Object.assign(childData,temp);
-      delete finalData.password;
-      //pushing merged data to items
-      this.items.push(finalData);
-        }).catch(e=>{
-          console.log("");
-        });
-    }).catch(e=>{
-          console.log("");
-        }); 
+      }).catch(e => {
+        console.log("");
+      });
   }
 
 
   //Initiate Idol Array so that it doesnt get pushed multiple times
-  getIdolArray(){
+  getIdolArray() {
     this.idolArray = this.items;
-    console.log('idolArray:',this.idolArray);
-  } 
+  }
 
 
   //Initiate at Start of page
 
-  ngOnInit(){
+  ngOnInit() {
     this.getIdols();
     this.getIdolArray();
   }
 
-  ionViewWillLoad(){
-    
-  }
+  ionViewWillLoad() {}
 
 
   //Adding function to follow button
-  followButtonFunc(idolKey:any,data:any){
-    var user = firebase.auth().currentUser;
-    delete data.followColor;
-    firebase.database().ref("following/").child(user.uid).child(idolKey).set(data);
-    console.log(data);
+  followButtonFunc(idolKey: any, data: any) {
+  this.searchProvider.followButtonFunc(idolKey,data);
+  }
+
+  setSubjUID(inpUID) {
+    this._subjectProvider.setSubjUID(inpUID);
+    this.navCtrl.push(TempProfilePage);
+  }
+
+  setFollowStyle(key){
+    var color:string = this.searchProvider.setFollowColor(key);
+    this.followStatus = this.searchProvider.setFollowStatus(key);
+    return color
   }
 
   //Search Function
   getItems(ev: any) {
     this.getIdolArray();
-    console.log('idolArray in getItems():',this.idolArray);
     let val = ev.target.value;
     // if the value is an empty string don't filter the items
     if (val && val.trim() != '') {
-        this.idolArray = this.idolArray.filter((item, username) => {
-            console.log(item);    
-            return (item.username.toLowerCase().indexOf(val.toLowerCase()) > -1);
-        })
+      this.idolArray = this.idolArray.filter((item, username) => {
+        return (item.username.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
     }
-  
-  }
-  
-  setSubjUID(inpUID){
-    this._subjectProvider.setSubjUID(inpUID);
-    this.navCtrl.push(TempProfilePage);
-  }
-
-  setIsFollowing(e){
-    // this.is_following = e;
-    return e
-  }
-
-  isUserFollowing(key){
-    console.log("im in isUserFollowing");
-    var is_following
-    var user = firebase.auth().currentUser;
-    firebase.database().ref("following").child(user.uid).on('value', snapshot =>{
-     
-    is_following = this.setIsFollowing(snapshot.hasChild(key));
-    console.log("idolUID",key);
-    console.log("is user following function",is_following);
-      });
-    if(is_following){
-      return "green";
-    }
-    else{
-      return "default"
-    }
-  }
-
-  // setFollowColor(key){
-  //   this.isUserFollowing(key);
-  //   console.log()
-  //   var followColor:string;
-  //   followColor = "default";
-  //   if(this.is_following){
-  //     followColor = "light";
-  //     console.log(followColor);
-  //   }
-  //   return followColor
-  // }
-
-  // navToUserProf(){
-  //    this.navCtrl.push(tempProfilePage);
-  // }
-  
-  ////////////////////////////////////// Adding and removing items ////////////////////////////////////////
-  addItem() {
-    //this.firebaseProvider.addItem('');
-    var user = firebase.auth().currentUser;
-
-    firebase.database().ref("following").child(user.uid).push("hello")
 
   }
- 
-  removeItem(id) {
-    this.firebaseProvider.removeItem(id);
-  }
-  ////////////////////////////                   End                     ///////////////////////////////////
 
 }
--- CelebApp1/src/providers/search-service/search-service.ts
+++ CelebApp1/src/providers/search-service/search-service.ts
@@ -1,4 +1,4 @@
-import { Injectable,NgZone } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import firebase from 'firebase';
 import { AngularFireAuth } from 'angularfire2/auth';
@@ -9,34 +9,66 @@ import { AngularFireModule } from 'angularfire2';
 export class SearchProvider {
 
   idolItems2: any;
-  public container= new Array();
- 
+  public container = new Array();
+
   constructor(public afd: AngularFireDatabase, public zone: NgZone) {
     this.idolItems2 = firebase.database().ref('idols');
 
   }
 
- 
-  addItem(name) {
-    this.afd.list('/following/').push(name);
-  }
- 
-  removeItem(id) {
-    this.afd.list('/idols/').remove(id);
-  }
-
-  followItem(id){
-    this.afd.list('/profile/')
-  }
-
-  getIdol(userId: any){
+  getIdol(userId: any) {
 
     var idolRef = this.idolItems2.child('userId');
-    return idolRef.once('value');{
+    return idolRef.once('value'); {
 
-      };
+    };
   }
-    
+
+  setIsFollowing(e) {
+    return e
+  }
+  
+  isFollowing(key) {
+    var is_following:boolean;
+    var user = firebase.auth().currentUser;
+    firebase.database().ref("following").child(user.uid).on('value', snapshot => {
+      is_following = this.setIsFollowing(snapshot.hasChild(key));
+
+    });
+    return is_following
+  }
+
+  followButtonFunc(idolKey: any, data: any) {
+    var user = firebase.auth().currentUser;
+    var ref = firebase.database().ref("following/").child(user.uid).child(idolKey)
+    var followRef = firebase.database().ref("following").child(user.uid);
+    delete data.followColor;
+      var is_following =this.isFollowing(idolKey);
+        if(is_following){
+          ref.remove();
+        }
+        else{
+          ref.set(data);
+        }
+
+  }
+  
+  setFollowStatus(key){
+    var is_following = this.isFollowing(key);
+    if(is_following){
+      return "Unfollow"
+    }
+    return "Follow"
+  }
+
+  setFollowColor(key) {
+    var is_following = this.isFollowing(key);
+    if (is_following) {
+      return "grey";
+    }
+    return "";
+  }
+
 }
 
 
